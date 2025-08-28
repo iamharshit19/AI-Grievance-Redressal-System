0abd5fd42d96e27f1733847fd58041401cb619e2
iamharshit19 <harshitgupta0901@gmail.com>
2025-08-28 17:12:30 +0530

    server.js cors issue fix

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,19 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
 
 app.use(json());
 app.use("/api/admin-dashboard", adminDashboardRoutes);
